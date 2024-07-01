0b356ade2fddaf015fec1a6ebc6df6239eb67de9
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-01 18:40:21 +0300

    updated user route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import { Router } from 'express';
 import { registerUser } from '../controllers/userController.js';
 
-const router = express.Router();
+const router = Router();
 
 router.post('/register', registerUser);
 
