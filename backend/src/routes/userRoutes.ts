fb914dbf761835613159f3e70565fda9cd0b1954
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-02 14:03:18 +0300

    updated user route with getUserProfile

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
 
 export default router;
