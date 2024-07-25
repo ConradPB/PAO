ff12d63cd9f37d8f96b276f8257833ff1abc710b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-25 20:11:18 +0300

    Added event model

---
+++ backend/src/models/Event.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IEvent extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  title: string;
+  description: string;
+  date: Date;
+  recurring: boolean;
+  frequency?: string;
+}
+
+const eventSchema: Schema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    recurring: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    frequency: {
+      type: String,
+      enum: ['daily', 'weekly', 'monthly'],
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Event = mongoose.model<IEvent>('Event', eventSchema);
+
+export default Event;
