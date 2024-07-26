be1defeb341e20603c06af84bd1653e22b8c83cc
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-26 23:11:34 +0300

    updated passport config

--- backend/src/config/passportConfig.ts
+++ backend/src/config/passportConfig.ts
@@ -1,34 +1,52 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
+import dotenv from 'dotenv';
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID!,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  callbackURL: "/auth/google/callback"
-},
-  async (accessToken, refreshToken, profile, done) => {
-    const existingUser = await User.findOne({ googleId: profile.id });
-    if (existingUser) {
-      return done(null, existingUser);
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name?.givenName,
+            lastName: profile.name?.familyName,
+            image: profile.photos?.[0]?.value,
+            email: profile.emails?.[0]?.value,
+          });
+          await user.save();
+        }
+
+        done(null, user);
+      } catch (error) {
+        done(error, undefined);
+      }
     }
-
-    const newUser = new User({
-      googleId: profile.id,
-      name: profile.displayName,
-      email: profile.emails?.[0].value,
-    });
-
-    await newUser.save();
-    done(null, newUser);
-  }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, (user as any).id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 });
+
+export default passport;
