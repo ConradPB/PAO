4923147898988f7f51f5d03e6b1defe78efd5ca3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-26 23:31:06 +0300

    updated user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,10 +1,10 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
+  googleId: string;
   name: string;
   email: string;
   password: string;
-  googleId: string;
 }
 
 const userSchema: Schema = new Schema(
