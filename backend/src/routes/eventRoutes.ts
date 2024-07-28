e9ab76e458b35459fb395328c711b394913522be
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-28 17:32:45 +0300

    updated

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import {
-  createEvent,
-  getEvents,
-  updateEvent,
-  deleteEvent
-} from '../controllers/eventController.js';
+import { createEvent, getEvents, getEventById, updateEvent, deleteEvent } from '../controllers/eventController.js';
+import { AuthenticatedRequest } from '../types/custom.js';
 
 const router = express.Router();
 
-router.route('/').post(protect, createEvent).get(protect, getEvents);
-router.route('/:id').put(protect, updateEvent).delete(protect, deleteEvent);
+router.route('/')
+  .post(protect, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => createEvent(req, res, next))
+  .get(protect, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => getEvents(req, res, next));
+
+router.route('/:id')
+  .get(protect, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => getEventById(req, res, next))
+  .put(protect, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => updateEvent(req, res, next))
+  .delete(protect, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => deleteEvent(req, res, next));
 
 export default router;
