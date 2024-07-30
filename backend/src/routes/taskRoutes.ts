03943a2e08670727ae914f8df269a2aa2b37a990
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-07-30 21:23:23 +0300

    updated

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,16 +1,30 @@
-import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import {
-  createTask,
-  getTasks,
-  updateTask,
-  deleteTask
+import { Router } from 'express';
+import { body } from 'express-validator';
+import { 
+  getTasks, 
+  createTask, 
+  getTaskById, 
+  updateTask, 
+  deleteTask 
 } from '../controllers/taskController.js';
+import protect from '../middleware/authMiddleware.js';
 
-const router = express.Router();
+const router = Router();
 
-router.route('/').post(protect, createTask).get(protect, getTasks);
-router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
+router.route('/')
+  .get(protect as any, getTasks as any)
+  .post(
+    protect as any,
+    [
+      body('title', 'Title is required').not().isEmpty(),
+      body('description', 'Description is required').not().isEmpty(),
+    ],
+    createTask as any
+  );
+
+router.route('/:id')
+  .get(protect as any, getTaskById as any)
+  .put(protect as any, updateTask as any)
+  .delete(protect as any, deleteTask as any);
 
 export default router;
-
