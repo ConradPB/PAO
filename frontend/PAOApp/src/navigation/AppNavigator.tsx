16982d8efddd972bc559eadd342c8a5295bd987b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-03 16:47:30 +0300

    updated

--- frontend/PAOApp/src/navigation/AppNavigator.tsx
+++ frontend/PAOApp/src/navigation/AppNavigator.tsx
@@ -3,11 +3,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 
-const Stack = createStackNavigator();
+// Define the type for the stack's parameters
+export type RootStackParamList = {
+  Login: undefined; // No params are passed to Login
+  SignUp: undefined; // No params are passed to SignUp
+};
+
+// Apply the type to the stack navigator
+const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
