175fd938d8cf97bfac01fe386f7d1ed9a2a3527f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-03 14:36:23 +0300

    update

--- frontend/PAOApp/App.tsx
+++ frontend/PAOApp/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from 'screens/LoginScreen';
+import SignUpScreen from 'screens/SignUpScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    </Stack.Navigator>
   );
-}
+};
+
+export default AppNavigator;
