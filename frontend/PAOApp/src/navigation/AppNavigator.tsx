0e5b19b321cdcf9d7035fc47c9004d6a5fd03a57
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-15 03:47:11 +0300

    update

--- frontend/PAOApp/src/navigation/AppNavigator.tsx
+++ frontend/PAOApp/src/navigation/AppNavigator.tsx
@@ -3,23 +3,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import HomeScreen from '../screens/HomeScreen';
 
-export type RootStackParamList = {
-  Login: undefined;
-  SignUp: undefined;
-  Welcome: undefined;
-  Home: undefined; 
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      {/* Add other screens here */}
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
