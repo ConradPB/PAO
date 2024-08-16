582f954c87733e5b4208bc44b2a56f8f769faeb0
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-16 23:32:07 +0300

    update

--- frontend/PAOApp/src/navigation/AppNavigator.tsx
+++ frontend/PAOApp/src/navigation/AppNavigator.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
-import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import MeetupsScreen from '../screens/MeetupsScreen';
-import DiscoverScreen from '../screens/DiscoverScreen';
-import NotificationsScreen from '../screens/NotificationsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import DiscoverScreen from '../screens/DiscoverScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import NotificationsScreen from '../screens/NotificationsScreen';
 import AccountSettingsScreen from '../screens/AccountSettingsScreen';
 import PrivacySettingsScreen from '../screens/PrivacySettingsScreen';
+import NotificationSettingsScreen from '../screens/NotificationSettingsScreen';
 import LanguageRegionSettingsScreen from '../screens/LanguageRegionSettingsScreen';
 
 
@@ -22,17 +23,18 @@ const Stack = createStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Meetups" component={MeetupsScreen} />
       <Stack.Screen name="Discover" component={DiscoverScreen} />
-      <Stack.Screen name="Notifications" component={NotificationsScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
       <Stack.Screen name="AccountSettings" component={AccountSettingsScreen} />
       <Stack.Screen name="PrivacySettings" component={PrivacySettingsScreen} />
+      <Stack.Screen name="NotificationSettings" component={NotificationSettingsScreen} />
       <Stack.Screen name="LanguageRegionSettings" component={LanguageRegionSettingsScreen} />
 
 
