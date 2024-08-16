8c1f797e56962efec3fe93da7e067bd6859aead5
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-16 18:21:10 +0300

    updated

---
+++ frontend/PAOApp/src/navigation/types.ts
@@ -0,0 +1,12 @@
+export type RootStackParamList = {
+    Login: undefined;
+    SignUp: undefined;
+    Welcome: undefined;
+    Home: undefined; 
+    Profile: undefined;
+    Meetups: undefined;
+    Discover: undefined;
+    Notifications: undefined;
+    AccountSettings: undefined;
+    Settings: undefined;
+  };
