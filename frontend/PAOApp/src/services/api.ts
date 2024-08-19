64ef546032d6ddf4aabd67fbd72c453b31e6c733
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-19 16:32:50 +0300

    updated

--- frontend/PAOApp/src/services/api.ts
+++ frontend/PAOApp/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000/api', // Ensure this is correct based on your environment
+  baseURL: 'http://10.0.2.2:7000/api', 
   headers: {
     'Content-Type': 'application/json',
   },
