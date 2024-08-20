2a16b2da3ca42a51b46ccb1188d853de556f70ee
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-20 21:47:40 +0300

    updated

--- frontend/PAOApp/src/screens/SignUpScreen.tsx
+++ frontend/PAOApp/src/screens/SignUpScreen.tsx
@@ -26,7 +26,7 @@ const SignUpScreen = () => {
 
     try {
       setIsLoading(true);
-      const response = await api.post('http://192.168.1.100:7000/api', {
+      const response = await api.post('http://192.168.1.100:8081/', {
         name,         
         email,
         password,
