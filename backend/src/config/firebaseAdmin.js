5fed4ff31a74d56df85b7846611b218fe5759733
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-31 01:23:30 +0300

    updated

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 import { join } from 'path';
 
 // Path to service account key
-const serviceAccount = require(join(__dirname, './path/to/your-service-account-file.json'));
+const serviceAccount = require(join(__dirname, './path/to/service-account-file.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
