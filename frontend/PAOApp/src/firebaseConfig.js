5b7dfdbbdc1aecc24b55ea1fd62f1f0161de6e0b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-08-31 01:25:28 +0300

    updated

---
+++ frontend/PAOApp/src/firebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseAdmin.js (Backend)
+import admin from 'firebase-admin';
+import { join } from 'path';
+
+// Path to service account key
+const serviceAccount = require(join(__dirname, './path/to/service-account-file.json'));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://PROJECT_ID.firebaseio.com"
+});
+
+export default admin;
