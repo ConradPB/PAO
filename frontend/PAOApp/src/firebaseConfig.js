5ac05f8e9e29d5d58f1746c2a895a3cd6ea88817
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-09-07 23:30:27 +0300

    updated

--- frontend/PAOApp/src/firebaseConfig.js
+++ frontend/PAOApp/src/firebaseConfig.js
@@ -2,12 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/messaging'; 
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
+  apiKey: "AIzaSyAop_tJ0EFm58tdTHdgImaJlg3762qJtiQ",
   authDomain: "paocommunity-14f72.firebaseapp.com",
   projectId: "paocommunity-14f72",
   storageBucket: "paocommunity-14f72.appspot.com",
   messagingSenderId: "628514439370",
-  appId: "APP_ID",
+  appId: "1:628514439370:web:4598a88627641f40c0f433",
+  measurementId: "G-13T511YR54"
 };
 
 if (!firebase.apps.length) {
